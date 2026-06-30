00e37ef28ad25a56fe408e235e9fd12411f00452
kavehzohrabi <kaveh.zohrabi915@gmail.com>
2026-06-30 15:04:47 +0330

    feat(role): create role models

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,6 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
